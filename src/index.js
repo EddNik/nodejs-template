3ce50494baba953261b0455d2b673688004adf70
Eduard <nikinna86@gmail.com>
2025-11-18 22:42:21 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-const message = 'Hello world Node';
+// const message = 'Hello world Node';
 
-console.log(message);
+import path from 'node:path';
+
+const somePath = path.join('some_folder', 'some_file.txt');
+
+// абсолютний шлях до робочої директорії
+const pathToWorkDir = path.join(process.cwd());
+
+// додаємо нові частини до шляху
+const pathToFile = path.join(pathToWorkDir, 'some_folder', 'some_file.txt');
+
+console.log('somePath', somePath);
+console.log('pathToFile', pathToFile); ///home/eduard/Develop/Go_IT_Projects/Nodejs/nodejs-template/some_folder/some_file.txt
